f76e810a8b83a444888737af4f10c198f090ae84
Owen <owen@howtank.com>
2021-02-25 14:23:59 +0100

    initializing node server

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 //IMPORTS
+require('dotenv').config();
+const express = require('express');
 
 
 // SERVER DEFINITION
 
 class ServerClass{
-    
+    constructor(){
+        this.server=express();
+        this.port=process.env.PORT;
+
+        
+
+    }
+    launch(){
+        this.server.listen(this.port, ()=>{
+            console.log({node:`http://localhost:${this.port}`})
+
+
+
+        })
+    }
 }
 
 // START SERVER
+
+const MyServer= new ServerClass();
+MyServer.launch();
